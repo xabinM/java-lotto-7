0311ce30bd4773690cc4b1115170d8828b62ba65
Min <binine2@gmail.com>
2024-11-03 16:36:55 +0900

    feat: 당첨 통계 출력 통합 프로세스 기능 구현

--- src/main/java/lotto/controller/Application.java
+++ src/main/java/lotto/controller/Application.java
@@ -2,12 +2,11 @@ package lotto.controller;
 
 import lotto.model.LottoMachine;
 import lotto.model.LottoNumberGenerator;
-import lotto.view.LottoView;
 
 public class Application {
     public static void main(String[] args) {
         LottoShop lottoMachine = new LottoShop(
-                new LottoView(), new LottoMachine(new LottoNumberGenerator()));
+                new LottoMachine(new LottoNumberGenerator()));
         lottoMachine.runLotto();
     }
 }
